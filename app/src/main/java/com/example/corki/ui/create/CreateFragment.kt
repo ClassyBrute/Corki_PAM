d4a284a6aec85bcaa0e386590db06130292c578e
ClassyBrute <315696@uwr.edu.pl>
2022-04-02 14:27:34 +0200

    refactoring files

--- app/src/main/java/com/example/corki/ui/create/CreateFragment.kt
+++ app/src/main/java/com/example/corki/ui/create/CreateFragment.kt
@@ -1,4 +1,4 @@
-package com.example.corki.ui.home
+package com.example.corki.ui.create
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,30 +7,29 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.corki.databinding.FragmentHomeBinding
+import com.example.corki.databinding.FragmentCreateBinding
 
-class HomeFragment : Fragment() {
+class CreateFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentCreateBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
 
-    //init
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
+        val createViewModel =
+                ViewModelProvider(this).get(CreateViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentCreateBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textCreate
+        createViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
--- app/src/main/java/com/example/corki/ui/my_offers/MyOffersFragment.kt
+++ app/src/main/java/com/example/corki/ui/my_offers/MyOffersFragment.kt
@@ -1,4 +1,4 @@
-package com.example.corki.ui.notifications
+package com.example.corki.ui.my_offers
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.corki.databinding.FragmentNotificationsBinding
+import com.example.corki.databinding.FragmentMyOffersBinding
 
-class NotificationsFragment : Fragment() {
+class MyOffersFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentMyOffersBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class NotificationsFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-                ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        val myOffersViewModel =
+                ViewModelProvider(this).get(MyOffersViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentMyOffersBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textMyOffers
+        myOffersViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
